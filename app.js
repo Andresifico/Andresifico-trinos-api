8356c1de7a59b01625445dc4e8d43b9d0978547b
Andresifico <amezaf@uninorte.edu.co>
2021-11-29 23:52:45 +0000

    feat:  🔨 app.js updated   tweetsRouter added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const ErrorSerializer = require('./src/serializers/BaseSerializer');
 const usersRouter = require('./src/routes/users');
+const tweetsRouter = require('./src/routes/tweets');
+// const comentsRouter = require('./src/routes/coments');
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', usersRouter);
+app.use('/tweets', tweetsRouter);
+// app.use('/coments', comentsRouter);
 
 app.use((req, res, next) => {
   res.status(404);
