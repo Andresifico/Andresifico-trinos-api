2ce25f360afa2bb3c28c2fe5ccef8bc4d25a0fd7
Andresifico <amezaf@uninorte.edu.co>
2021-11-28 19:29:55 -0500

    feat: 🔧 users routes added

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,18 +9,24 @@ const {
   deactivateUser,
   loginUser,
   getAllUsers,
+  sendEmailPassword,
+  updatePassword,
+  resetPasswWord,
+  logOut,
 } = require('../controllers/users');
 
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const { paginationMiddleware } = require('../middlewares/paginationMiddleware');
 
 router.get('/all', authMiddleware, paginationMiddleware, getAllUsers);
-
 router.post('/', createUser);
 router.post('/login', loginUser);
-
+router.post('/send_password_reset', sendEmailPassword);
 router.get('/:id', getUserById);
 router.put('/:id', authMiddleware, updateUser);
 router.delete('/:id', authMiddleware, deactivateUser);
+router.post('/update_password', authMiddleware, updatePassword);
+router.post('/reset_password', resetPasswWord);
+router.post('/logout', authMiddleware, logOut);
 
 module.exports = router;
